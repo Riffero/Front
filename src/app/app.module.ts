1562e723126f67918711a5142096afb019f77d5c
Mauricio D <mauriciodawbarn88@gmail.com>
2018-07-31 14:40:46 -0300

    Lista de usuarios creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
